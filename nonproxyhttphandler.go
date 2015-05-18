fcb0f8106f7caba13576f781068341c808542bae
William Bailey <mail@williambailey.org.uk>
2015-05-18 16:54:49 +0100

    Provide both wpad.dat and proxy.pac http endpoints.

--- nonproxyhttphandler.go
+++ nonproxyhttphandler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -17,6 +18,20 @@ func NewNonProxyHTTPHandler(pac *Pac) http.Handler {
 		fmt.Fprintf(w, "%s v%s %s\n", Name, Version, pac.PacFilename())
 	})
 	router.GET("/wpad.dat", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		w.Header().Set("Content-Type", "application/x-ns-proxy-autoconfig")
+		wpad := strings.Replace(`
+function FindProxyForURL(url, host)
+{
+	if (isInNet(host, "127.0.0.0", "255.0.0.0"))
+	{
+		return "DIRECT";
+	}
+	return "PROXY %%HOST%%";
+}
+`, "%%HOST%%", r.Host, -1)
+		w.Write([]byte(wpad))
+	})
+	router.GET("/proxy.pac", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 		w.Header().Set("Content-Type", "application/x-ns-proxy-autoconfig")
 		w.Write(pac.PacConfiguration())
 	})
